95bbe8bac6f4cb52be8f644e1ba9d2637147393e
rikkoRicardo <ricardofonseca673@gmail.com>
2021-11-20 20:46:35 +0000

    Create GCD - Greatest Common Divisor.cpp

---
+++ GCD - Greatest Common Divisor.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+#include <cstdlib>
+//greatest common divisor
+
+long long GCD(long a,long b) {
+	long best = 0;
+	for (int i = 0; i < (a + b); i++) {
+		if (a % i == 0 && b % i == 0) {
+			best = i;
+		}
+	}
+	return best;
+}
+
+int main()
+{
+std::cout << GCD(rand() % 10000 + 1000,rand()%10000 +1000)
+}
+
+
