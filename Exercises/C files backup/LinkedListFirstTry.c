9a3f27ae7f7b2d19ff2e55edefb1874a4762f927
Ricardo <93994704+rikkoRicardo@users.noreply.github.com>
2022-10-23 23:04:52 +0100

    Update LinkedListFirstTry.c

--- Exercises/C files backup/LinkedListFirstTry.c
+++ Exercises/C files backup/LinkedListFirstTry.c
@@ -1,4 +1,5 @@
 // FILE_NAME.C
+// http://cslibrary.stanford.edu/103/LinkedListBasics.pdf
 // made by rikko
 #include <stdio.h>
 #include <stdlib.h>
@@ -24,7 +25,24 @@ void printlist(Node *head)
   printf("%d items\n", counter);
 }
 
-void insert_start(Node **head, int value)
+Node *DummyList()
+{
+  Node dummy;          // Dummy is temporarely the first node
+  Node *tail = &dummy; // Start the node at dummy
+                       // build the list on dummy.next (tail->next)
+
+  dummy.next = NULL;
+
+  for (int i = 0; i < 6; i++)
+  {
+    Push(&(tail->next), i);
+    tail = tail->next;
+  }
+  // 1 - 2 - 3 - 4 - 5 - Counter: 5
+  printf("The last node num is %d\n", tail->value);
+}
+
+void Push(Node **head, int value)
 {
   Node *newNode = (Node *)malloc(sizeof(Node));
   newNode->value = value;
@@ -34,6 +52,19 @@ void insert_start(Node **head, int value)
 
   // Change the head from the old to the new start
   *head = newNode;
+}
+
+void insert_after(Node* previous_node,int value) {
+    Node* new_node = (Node*)malloc(sizeof(Node));
+
+    if (new_node == NULL)
+        return;
+    
+    new_node->value = value;
+    //change the next node to the sucessor
+    new_node->next = previous_node->next;
+    //change the predecessor's node to the new node
+    previous_node->next = new_node;
 }
 
 void insert_end(Node **head, int value)
@@ -64,6 +95,20 @@ void insert_end(Node **head, int value)
   curr->next = tmp;
 }
 
+void Deallocate(Node **head)
+{
+  Node *curr = *head;
+  while (curr != NULL)
+  {
+    // get handle
+    Node *nod = curr;
+    // move curr
+    curr = curr->next;
+    //  deallocate using nod
+    free(nod);
+  }
+}
+
 Node *BuildTwoThree()
 {
   Node *head = (Node *)malloc(sizeof(Node)); // Allocate 2 nodes in the heap
@@ -84,13 +129,15 @@ int main()
   // Node *head = BuildTwoThree();
   // Where the linked list starts
   Node *head = NULL;
-
+  Node *tail = NULL;
   // Inserts numbers from 1 to 10
   for (int i = 1; i < 10; i++)
-    insert_end(&head, i);
+    insert_end(&head, &tail, i);
 
   // Prints The inserted numbers
   printlist(head);
 
+  // Deallocate list
+  Deallocate(head);
   return 0;
 }
